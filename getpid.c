e681e768e8b1c819bfd73b33c52cb2ebc235e718
Mohammed Rerhaye <Mr.rghay@gmail.com>
2024-08-15 16:21:36 +0100

    PID & PPID

---
+++ getpid.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <stdio.h>
+
+
+int main (int ac, char **av)
+{
+    (void) ac;
+    int i=0;
+
+    while(av[i])
+    {
+        printf("%s\n", av[i]);
+        i++; 
+    }
+    return (0);
+}
